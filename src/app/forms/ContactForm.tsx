b8cf545d014fed1b9174eeb1250a7b5d743e71dd
ArcidesFerrao <cidesferrao@gmail.com>
2025-05-22 21:22:44 +0200

    contact form to email

--- src/app/forms/ContactForm.tsx
+++ src/app/forms/ContactForm.tsx
@@ -1,22 +1,11 @@
 "use client";
 
-// import { useState } from "react";
-
 const ContactForm = () => {
-  // const [data, setData] = useState({ name: "", email: "", message: "" });
-
-  // const handleChange = (e: any) =>
-  //   setData({ ...data, [e.target.name]: e.target.value });
-
-  // const handleSubmit = (e: any) => {
-  //   e.preventDefault();
-  //   alert("Form submitted! You can link this to Formspree or your API.");
-  // };
-
   return (
     <form
       id="contact"
-      // onSubmit={handleSubmit}
+      action="https://formsubmit.co/cidesferrao@gmail.com"
+      method="POST"
       className="contact-form flex flex-col items-center gap-5"
     >
       <h2 className="text-4xl font-bold bg-clip-text ">Contact Me</h2>
@@ -26,23 +15,11 @@ const ContactForm = () => {
         Let’s talk about your project. I usually respond in less than 24 hours.
       </p>
       <div className="flex flex-col gap-5">
-        <input
-          name="name"
-          placeholder="Your Name"
-          // onChange={handleChange}
-          required
-        />
-        <input
-          name="email"
-          type="email"
-          placeholder="Your Email or WhatsApp"
-          // onChange={handleChange}
-          required
-        />
+        <input name="name" placeholder="Your Name" required />
+        <input name="email" type="email" placeholder="Your Email" required />
         <textarea
           name="message"
           placeholder="What do you want built?"
-          // onChange={handleChange}
           required
         />
         <button type="submit" className="btn">
