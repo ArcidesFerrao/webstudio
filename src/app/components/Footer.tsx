cb02e607740c36205eb2d32c20c55b6410e77530
ArcidesFerrao <cidesferrao@gmail.com>
2025-05-25 15:11:46 +0200

    footer logo

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,15 +1,26 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const Footer = () => (
   <>
-    <footer className="w-full flex space-x-6 justify-center py-8 my-10">
-      <div className="flex gap-2">
-        <p>Email:</p>
-        <Link href="">cidesferrao@gmail.com</Link>
+    <footer className="w-full flex items-center space-x-8 justify-center py-8 my-10">
+      <div>
+        <Image
+          src="/assets/logo-ws3.png"
+          width={180}
+          height={100}
+          alt="web studio logo"
+        />
       </div>
-      <div className="flex gap-2">
-        <p>Whatsapp:</p>
-        <Link href="">+258 85 27 40 554</Link>
+      <div className="flex flex-col justify-between h-full gap-1">
+        <div className="flex gap-2">
+          <p>Email:</p>
+          <Link href="">cidesferrao@gmail.com</Link>
+        </div>
+        <div className="flex gap-2">
+          <p>Whatsapp:</p>
+          <Link href="">+258 85 27 40 554</Link>
+        </div>
       </div>
     </footer>
   </>
